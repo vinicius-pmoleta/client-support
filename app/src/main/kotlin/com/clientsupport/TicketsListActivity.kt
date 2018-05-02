f7a843670a6812927571f6f3c05d742418dceaf3
vinciusmoleta <vinicius.moleta@mttnow.com>
2018-05-02 22:50:32 +0100

    Initial structure

---
+++ app/src/main/kotlin/com/clientsupport/TicketsListActivity.kt
@@ -0,0 +1,12 @@
+package com.clientsupport
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TicketsListActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_tickets_list)
+  }
+}
